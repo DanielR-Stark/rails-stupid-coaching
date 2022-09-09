1e5d3b2c68e73e7efcac49624310c648ef5f4b95
daniel rivera daniel.rivera0823@gmail.com <daniel.rivera0823@gmail.com>
2022-09-08 20:41:18 -0500

    Funciona pero feo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def ask
+    # r
+  end
+
+  def answer
+    if params[:question] == 'I am going to work'
+       @answer = "Great!"
+    elsif params[:question] =~ /.+\?/
+       @answer = 'Silly question, get dressed and go to work!.'
+    else
+       @answer = 'I dont care, get dressed and go to work!'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/ask'
+  get 'ask', to: 'pages#ask'
+  get 'answer', to: 'pages#answer'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
